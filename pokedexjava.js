f2a89baa4d70252225f4f48fe5c8942bdd152cf5
edoardo <edolops.el@gmail.com>
2024-11-15 02:20:24 +0100

    finito progetto, migliorabbile

--- pokedexjava.js
+++ pokedexjava.js
@@ -20,24 +20,38 @@ const addPokemon = async ()=>{
     const li=document.createElement("li")
     const checkbox=document.createElement("input")
     const newButton = document.createElement("button")
+    const a = document.createElement("a")
+    const div = document.createElement("div")
+    const p = document.createElement("p")
 
     img.src = data.sprites.front_default
     img.alt = data.name
+    a.href = `https://wiki.pokemoncentral.it/${name}`
 
     checkbox.setAttribute("type", "checkbox")
-    newButton.innerText="Remove"
+    newButton.classList.add("bottone-nuovo")
     
-    li.innerText = name
+    newButton.innerText="Rimuovi"
+    p.innerText = `Nome Pokémon: ${name}`
+    a.innerText = "Wiki"
    
     ul.appendChild(li)
-    li.appendChild(checkbox)
+    li.appendChild(p)
+    li.appendChild(div)
+    div.appendChild(img)
     li.appendChild(newButton)
-    li.appendChild(img)
+    li.appendChild(a)
+    
 
     newButton.addEventListener("click", () => {
         ul.removeChild(li)
         input.value="";
     })
+
+    input.addEventListener("click", ()=> {
+        input.value="";
+    })
+
     }catch(error){
         console.error(error)
     }
